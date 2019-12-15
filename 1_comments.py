bf46bf19435faaa425971c5f72a09c90a621a896
sac4service <43067511+sac4service@users.noreply.github.com>
2019-12-15 18:09:12 +0530

    Create 1_comments.py

---
+++ 1_comments.py
@@ -0,0 +1,9 @@
+#This is single line comment
+
+'''This is multi-line comment
+Now you can write your comments 
+in multiple lines'''
+
+"""This is multi-line comment
+Now you can write your comments 
+in multiple lines"""
