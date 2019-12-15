f0f37f6e0b4f060197482777aef265888df695e2
sac4service <43067511+sac4service@users.noreply.github.com>
2019-12-15 19:12:45 +0530

    Update 2_firstprogram.py

--- 2_firstprogram.py
+++ 2_firstprogram.py
@@ -6,7 +6,9 @@ print('''This is help to
 display message in 
 multiple lines''')
 
-# ypu can use " instead of '
+print("""This is help to
+display message in 
+multiple lines""")
 
 '''The above one is help to 
 print the statement in python 3.x'''
