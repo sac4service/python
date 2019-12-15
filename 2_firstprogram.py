2b9a33b79eb85794822ed3479e0f47e63703d874
sac4service <43067511+sac4service@users.noreply.github.com>
2019-12-15 18:35:25 +0530

    Create 2_firstprogram.py

---
+++ 2_firstprogram.py
@@ -0,0 +1,15 @@
+print('Hello, World!')
+
+print("Hello, SAC")
+
+print('''This is help to
+display message in 
+multiple lines''')
+
+# ypu can use " instead of '
+
+'''The above one is help to 
+print the statement in python 3.x'''
+
+
+'''Thanking you for your visit'''
